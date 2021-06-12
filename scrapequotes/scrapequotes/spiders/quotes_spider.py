e6789b9050cb8af229405d869625f4c7a73e7098
gustavo-luz <gustavo.luz.unb@gmail.com>
2021-06-12 12:53:13 -0300

    stored in container

--- scrapequotes/scrapequotes/spiders/quotes_spider.py
+++ scrapequotes/scrapequotes/spiders/quotes_spider.py
@@ -1,4 +1,7 @@
 import scrapy
+from ..items import ScrapequotesItem
+
+
 # classe que vai herdar das classes criadas
 class QuoteSpider (scrapy.Spider):
     name = 'quotes'
@@ -9,6 +12,8 @@ class QuoteSpider (scrapy.Spider):
     #o response tem o codigo fonte do site, é de lá que se extrai
     def parse(self, response):
         #retornar a primeira observação de cada
+        items = ScrapequotesItem()
+
         all_div_quotes = response.css('div.quote')
         #1 quote por 1
         for quotes in all_div_quotes:
@@ -20,9 +25,9 @@ class QuoteSpider (scrapy.Spider):
             #tags: pega so a classe tags
             tag = quotes.css('.tag::text').extract()
 
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag    
             # mostra como um dicionário
             # yield é como um return, mas ela é usada com gerador que é usado pelo scrapy behind the scenes
-            yield {'titletext':title,
-                    'author':author,
-                    'tag':tag
-            }
+            yield items
